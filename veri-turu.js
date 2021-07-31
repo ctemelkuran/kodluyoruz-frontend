854074849c73ca00880c9f5fcb8ddcc1626be979
Cigir Temelkuran <91200000641@ogrenci.ege.edu.tr>
2021-07-31 21:06:50 +0300

    parseint

--- veri-turu.js
+++ veri-turu.js
@@ -4,3 +4,7 @@ let stringPrice = "111"
 console.log("price: ", typeof(price))
 
 console.log("string price: ", typeof(stringPrice))
+
+let number1 = "11"
+number1 = parseInt(number1) // int e dönüştür
+
