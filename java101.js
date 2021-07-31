00e327d153af4fd4f502333432375a2cb0e73896
Cigir Temelkuran <91200000641@ogrenci.ege.edu.tr>
2021-07-31 21:03:46 +0300

    veri türü js

---
+++ java101.js
@@ -0,0 +1,15 @@
+
+function sum(a, b) {
+    return a + b;
+}
+"Nice!".length;
+language = "Javascript";
+language[0]; //first character
+
+console.log(language[1]);
+
+const language = "JavaScript";
+//string.substring(indexStart, indexEnd) 
+language.substring(1, 4); //"ava"
+
+`I am learning ${language}`; //"I am learning JavaScript";
---
+++ veri-turu.js
@@ -0,0 +1,6 @@
+let price = 10;
+let stringPrice = "111"
+
+console.log("price: ", typeof(price))
+
+console.log("string price: ", typeof(stringPrice))
