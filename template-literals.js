82223a28b78b9eff26351750e7edfe84960003d0
Cigir Temelkuran <91200000641@ogrenci.ege.edu.tr>
2021-08-02 17:05:56 +0300

    index ve slice

--- template-literals.js
+++ template-literals.js
@@ -11,3 +11,21 @@ Yaşın: ${(new Date().getFullYear() - 1997)}
 kısa isim: ${username[0]}.`
 
 console.log(info)
+
+// Upper, lower case
+console.log(username.toUpperCase())
+console.log(username.toLowerCase())
+
+// Search
+console.log(email.search("@"))
+console.log(email[11])
+console.log(email.search("olmayan")) // -1 getirir
+
+// Belli bir kısmı almak
+let domainPart = email.slice(email.search("@")+1)
+console.log(domainPart)
+
+// index bulma ve sadece kodluyoruz kısmını al
+console.log(domainPart.slice(0, domainPart.indexOf('.')))
+
+//
